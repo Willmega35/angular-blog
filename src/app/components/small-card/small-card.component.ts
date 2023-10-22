b091f13518893902a6457a334dd40b8ddbb5f1b9
Willard Silveira Morais <will.dev2022@gmail.com>
2023-10-22 18:10:31 -0300

    Conteudo do meu Site adicinado

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./small-card.component.css']
 })
 export class SmallCardComponent implements OnInit {
+  @Input()
+  photoCover:string = "https://nosbastidores.com.br/wp-content/uploads/2021/08/Series-Marvel-Disney-Plus-1024x576-1.jpg"
+  @Input()
+  cardTitle:string = "NOVA SÉRIE ANUNCIADA"
 
   constructor() { }
 
